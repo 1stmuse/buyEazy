f62a74210ebfd3ef2d0b0c33d92aa54c73ed854c
1stmuse <olysegs@gmail.com>
2022-04-04 15:14:10 +0100

    more test

--- app/onBoarding/AuthButtons.js
+++ app/onBoarding/AuthButtons.js
@@ -1,10 +1,9 @@
 import * as React from "react";
 import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
-// import { useNavigation } from "@react-navigation/core";
 import Colors from "../Colors";
 
 const AuthButtons = ({ navigation }) => {
-  //   const navigation = useNavigation();
+  //  
   return (
     <View style={styles.container}>
       <TouchableOpacity
